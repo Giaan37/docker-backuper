2d796b7ef06fd5eca81fdfcca5c81031244aa278
Giaan37 <54108157+Giaan37@users.noreply.github.com>
2024-12-07 20:26:25 +0100

    Update backup_docker.sh

--- backup_docker.sh
+++ backup_docker.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 # Variables
-BACKUP_DIR="/path/backups/"  # Change this to your desired backup directory
+BACKUP_DIR="/media/glmds/7c541507-377b-486e-ba42-3d4daeb9aeb9/backups/"  # Change this to your desired backup directory
 DATE=$(date +"%Y%m%d_%H%M%S")
-BACKUP_IMAGES="$BACKUP_DIR/docker_images_$DATE.tar"
-BACKUP_VOLUMES="$BACKUP_DIR/docker_volumes_$DATE"
-BACKUP_CONTAINERS="$BACKUP_DIR/docker_containers_$DATE"
+BACKUP_IMAGES="$BACKUP_DIR/images/"
+BACKUP_VOLUMES="$BACKUP_DIR/volumes/"
+BACKUP_CONTAINERS="$BACKUP_DIR/containers_$DATE/"
 
-# Create backup directory if it doesn't exist
-mkdir -p "$BACKUP_DIR"
-mkdir -p "$BACKUP_VOLUMES"
-mkdir -p "$BACKUP_CONTAINERS"
+# Create backup directories if they don't exist
+mkdir -p "$BACKUP_IMAGES" "$BACKUP_VOLUMES" "$BACKUP_CONTAINERS"
 
 # Step 1: Backup Docker Images
 echo "Backing up Docker images..."
@@ -19,14 +17,13 @@ total_images=${#image_list[@]}
 count=0
 
 for image_id in "${image_list[@]}"; do
-  image_name=$(docker inspect --format '{{.RepoTags}}' "$image_id" | tr -d '[],"' | sed 's/\//_/g' | sed 's/:/_/g')  # Format the image name
-  echo "Backing up image: $image_name"
-  docker save "$image_id" -o "$BACKUP_IMAGES/$image_name.tar"
-  
-  # Calculate progress and display percentage
-  count=$((count + 1))
-  progress=$((count * 100 / total_images))
-  echo -ne "Progress: $progress% \r"
+    image_name=$(docker inspect --format '{{.RepoTags}}' "$image_id" | tr -d '[],"' | sed 's/\//_/g' | sed 's/:/_/g')
+    echo "Backing up image: $image_name"
+    docker save "$image_id" -o "$BACKUP_IMAGES/$image_name.tar" && echo "Saved $image_name"
+
+    count=$((count + 1))
+    progress=$((count * 100 / total_images))
+    echo -ne "Progress: $progress% \r"
 done
 echo -e "\nDocker images backed up to $BACKUP_IMAGES"
 
@@ -37,13 +34,13 @@ total_volumes=${#volume_list[@]}
 count=0
 
 for volume in "${volume_list[@]}"; do
-  echo "Backing up volume: $volume"
-  docker run --rm -v "$volume":/volume -v "$BACKUP_VOLUMES":/backup alpine tar czf "/backup/$volume.tar.gz" -C / volume
+    echo "Backing up volume: $volume"
+    docker run --rm -v "$volume":/volume -v "$BACKUP_VOLUMES":/backup alpine sh -c \
+        "cd /volume && tar czf /backup/$volume.tar.gz ."
 
-  # Calculate progress and display percentage
-  count=$((count + 1))
-  progress=$((count * 100 / total_volumes))
-  echo -ne "Progress: $progress% \r"
+    count=$((count + 1))
+    progress=$((count * 100 / total_volumes))
+    echo -ne "Progress: $progress% \r"
 done
 echo -e "\nDocker volumes backed up to $BACKUP_VOLUMES"
 
@@ -54,15 +51,13 @@ total_containers=${#container_list[@]}
 count=0
 
 for container in "${container_list[@]}"; do
-  echo "Backing up container: $container"
-  docker export "$container" -o "$BACKUP_CONTAINERS/$container.tar"
+    echo "Backing up container: $container"
+    docker export "$container" -o "$BACKUP_CONTAINERS/$container.tar"
 
-  # Calculate progress and display percentage
-  count=$((count + 1))
-  progress=$((count * 100 / total_containers))
-  echo -ne "Progress: $progress% \r"
+    count=$((count + 1))
+    progress=$((count * 100 / total_containers))
+    echo -ne "Progress: $progress% \r"
 done
 echo -e "\nDocker containers backed up to $BACKUP_CONTAINERS"
 
 echo "Backup completed!"
-
